30950cac91522e2e8c79a626671f22b967feb454
amartarocks3 <amartarocks3@gmail.com>
2022-04-05 14:01:21 +0530

    Added: Repeat till 100

---
+++ repeat100.java
@@ -0,0 +1,28 @@
+package snakeladder;
+
+public class repeat100 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int position=0;
+			while(position<100) {
+				int doornot=(int)Math.floor(Math.random()*10)%3;
+				int roll=(int)Math.floor(Math.random()*10)%6;
+				int temp=position;
+				
+				if(doornot==1) {
+					
+					position = position + roll;
+				}
+				else if(doornot==2) {
+					position = position - roll;
+				}
+				
+				System.out.println(position);
+				}
+
+		}
+		}
+
+
+
