191546bdd81d3c3f5bc5e5255076314e26577500
amartarocks3 <amartarocks3@gmail.com>
2022-04-05 13:17:46 +0530

    Added : Single player

---
+++ singleplayer.java
@@ -0,0 +1,12 @@
+package snakeladder;
+
+public class singleplayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int position=0;
+		System.out.println(position);
+		
+	}
+
+}
